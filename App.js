8ecb2a42b59b18cdd10d27fe60ceca6842e92362
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-31 19:31:47 +0900

    fix: react-native-webview import문 누락 문제 해결

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from "react-native-webview";
 
 export default function App() {
-  return (
-    <WebView
-    source={{ uri: 'https://tify-client.vercel.app' }}
-  />
-  );
+  return <WebView source={{ uri: "https://tify-client.vercel.app" }} />;
 }
-
