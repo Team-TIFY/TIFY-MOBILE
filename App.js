e3fa705c3d08eed89a6e5806162cc87648b603c0
Eugene Kim <eugene02876@gmail.com>
2023-12-31 15:40:28 +0900

    chore: 초기 세팅

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <WebView
+    source={{ uri: 'https://tify-client.vercel.app' }}
+  />
+  );
+}
+
